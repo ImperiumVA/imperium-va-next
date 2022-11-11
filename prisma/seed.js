4697e702ba20a693cd317ddc78e089afd80bc638
Mike H. DeVita <mike@devita.co>
2022-11-10 17:32:40 -0700

    wip

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,98 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+const SeedData = require('./seed-data.js');
 
-const roles = [
-    {
-        name: 'Admin',
-        slug: 'admin',
-    },
-    {
-        name: 'User',
-        slug: 'user',
-    },
-    {
-        name: 'VA Pilot',
-        slug: 'va-pilot'
-    },
-    {
-        name: 'VA Dispatcher',
-        slug: 'va-dispatcher'
-    },
-];
-
-const menus = [
-    {
-        slug: 'main-menu',
-        name: 'Main Menu',
-        order: 0,
-        adminOnly: false,
-        isRemovable: false,
-        isDisabled: false,
-        isAuthRequired: true,
-        items: [
-            {
-                slug: 'home',
-                name: 'Home',
-                href: '/',
-                label: 'Home',
-                isAuthRequired: false,
-                adminOnly: false,
-            },
-            {
-                slug: 'dashboard',
-                name: 'Dashboard',
-                href: '/dashboard',
-                label: 'Dashboard',
-                isAuthRequired: true,
-                adminOnly: false,
-            },
-            {
-                slug: 'onair',
-                name: 'OnAir',
-                href: '/onair',
-                label: 'OnAir',
-                isAuthRequired: true,
-                isDisabled: true,
-                adminOnly: false,
-            },
-        ]
-    },
-    {
-        slug: 'admin-menu',
-        name: 'Admin Menu',
-        order: 0,
-        adminOnly: true,
-        isRemovable: false,
-        isDisabled: false,
-        isAuthRequired: true,
-        items: [
-            {
-                slug: 'manage-users',
-                name: 'Manage Users',
-                href: '/admin/users',
-                label: 'Manage Users',
-                isAuthRequired: true,
-                adminOnly: true,
-            },
-            {
-                slug: 'manage-menus',
-                name: 'Manage Menus',
-                href: '/admin/menus',
-                label: 'Manage Menus',
-                isAuthRequired: true,
-                adminOnly: true,
-            },
-            {
-                slug: 'config',
-                name: 'Config',
-                href: '/admin/config',
-                label: 'Config',
-                isAuthRequired: true,
-                adminOnly: true,
-            },
-        ]
-    }
-]
 
 async function main() {
     const {
@@ -111,7 +20,7 @@ async function main() {
         console.log('App Config created');
     }
 
-    // roles.forEach(async (role) => {
+    // SeedData['roles'].forEach(async (role) => {
     //     const x = await prisma.role.upsert({
     //         where: {
     //             slug: role.slug,
@@ -122,7 +31,7 @@ async function main() {
     //     console.log(`Role ${x.name} upserted ✅`)
     // })
 
-    menus.forEach(async (menu) => {
+    SeedData['menus'].forEach(async (menu) => {
         const x = await prisma.menu.upsert({
             where: {
                 slug: menu.slug,
