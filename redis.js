c2433713b14d836c2321feafcd038bb415b8d751
Mike H. DeVita <mike@devita.co>
2022-11-11 14:32:08 -0700

    VAEvents redis queue

--- redis.js
+++ redis.js
@@ -1,4 +1,22 @@
-import IORedis from 'ioredis';
-export const Redis = new IORedis();
+import * as dotenv from 'dotenv'
+dotenv.config()
+import Redis from "ioredis"
 
-export default Redis;
+const {
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_PASSWORD
+} = process.env
+console.log({
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_PASSWORD
+});
+
+export const redis = new Redis({
+    host: REDIS_HOST || 'localhost',
+    port: Number(REDIS_PORT) || 6379,
+    password: REDIS_PASSWORD || '',
+});
+
+export default redis;
